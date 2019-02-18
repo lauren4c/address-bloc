c0aa009a09a84e3212ad6cc1105c4d4fab3f3c26
Lauren Forcey <lauren4c@gmail.com>
2019-02-18 10:33:30 -0700

    Completed Testing Assignment.

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -16,4 +16,10 @@ describe("MenuController", () => {
             expect(this.menu.getContactCount()).toBe(1);
     });
 });
+
+    describe("#remindMe()", () => {
+        it("Should return a string containing the text Learning is a life-long pursuit.", () => {
+            expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit.');
+        })
+    })
 });
