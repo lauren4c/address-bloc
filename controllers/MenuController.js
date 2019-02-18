4e44a39fa757dae176289c2dc0f86362b67dd939
Lauren Forcey <lauren4c@gmail.com>
2019-02-18 09:02:32 -0700

    Completed Prompt & Menu Checkpoint work.

---
+++ controllers/MenuController.js
@@ -0,0 +1,51 @@
+const inquirer = require("inquirer");
+
+module.exports = class MenuController {
+  constructor() {
+    this.mainMenuQuestions = [
+      {
+        type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: ["Add new Contact", "Exit"]
+      }
+    ];
+    this.contacts = [];
+  }
+
+  main() {
+    console.log("Welcome to AddressBloc!");
+    inquirer
+      .prompt(this.mainMenuQuestions)
+      .then(response => {
+        switch (response.mainMenuChoice) {
+          case "Add new Contact":
+            this.addContact();
+            break;
+          case "Exit":
+            this.exit();
+          default:
+            console.log("Invalid Input");
+            this.main();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  clear() {
+    console.log("\x1Bc");
+  }
+
+  addContact() {
+    this.clear();
+    console.log("addContact Called");
+    this.main();
+  }
+
+  exit() {
+    console.log("Thanks for using AddressBloc@");
+    process.exit();
+  }
+};
