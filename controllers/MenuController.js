ba4fa1ccb57ef91589983ad2bcdf8e30a2966577
Lauren Forcey <lauren4c@gmail.com>
2019-02-22 08:53:01 -0700

    Completed Listing Contact & Searching CP work.

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const ContactController = require("./ContactController");
+const moment = require("moment");
 
 module.exports = class MenuController {
   constructor() {
@@ -8,7 +9,13 @@ module.exports = class MenuController {
         type: "list",
         name: "mainMenuChoice",
         message: "Please choose from an option below: ",
-        choices: ["Add new Contact", "Exit"]
+        choices: [
+          "Add new Contact",
+          "View all Contacts",
+          "Search for a Contact",
+          "Show Current Date & Time",
+          "Exit"
+        ]
       }
     ];
     this.book = new ContactController();
@@ -22,6 +29,15 @@ module.exports = class MenuController {
         switch (response.mainMenuChoice) {
           case "Add new Contact":
             this.addContact();
+            break;
+          case "View all Contacts":
+            this.getContacts();
+            break;
+          case "Search for a Contact":
+            this.search();
+            break;
+          case "Show Current Date & Time":
+            this.getDate();
             break;
           case "Exit":
             this.exit();
@@ -56,11 +72,99 @@ module.exports = class MenuController {
     });
   }
 
+  getDate() {
+    console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    this.main();
+  }
+
   exit() {
     console.log("Thanks for using AddressBloc!");
     process.exit();
   }
   getContactCount() {
     return this.contacts.length;
+  }
+  getContacts() {
+    this.clear();
+    this.book
+      .getContacts()
+      .then(contacts => {
+        for (let contact of contacts) {
+          this._printContact(contact);
+        }
+        this.main();
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
+  }
+
+  search() {
+    inquirer
+      .prompt(this.book.searchQuestions)
+      .then(target => {
+        this.book.search(target.name).then(contact => {
+          if (contact === null) {
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
+  }
+  showContact(contact) {
+    this._printContact(contact);
+    inquirer
+      .prompt(this.book.showContactQuestions)
+      .then(answer => {
+        switch (answer.selected) {
+          case "Delete contact":
+            this.delete(contact);
+            break;
+          case "Main menu":
+            this.main();
+            break;
+          default:
+            console.log("Something went wrong.");
+            this.showContact(contact);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.showContact(contact);
+      });
+  }
+  _printContact(contact) {
+    console.log(`
+    name: ${contact.name}
+    phone number: ${contact.phone}
+    email: ${contact.email}
+    -------------------------`);
+  }
+
+  delete(contact) {
+    inquirer
+      .prompt(this.book.deleteConfirmQuestions)
+      .then(answer => {
+        if (answer.confirmation) {
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
   }
 };
--- db/migrations/20190219144508-create-contact.js
+++ db/migrations/20190219144508-create-contact.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Contacts', {
+    return queryInterface.createTable("Contacts", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Contacts');
+    return queryInterface.dropTable("Contacts");
   }
 };
